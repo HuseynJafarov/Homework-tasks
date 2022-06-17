e190ef5d320d4c84fa370d16ec93b231d2687a40
HuseynJafarov <huseynnj@code.edu.az>
2022-06-17 15:17:25 +0400

    Homeworks

---
+++ HomeWork(diagram)/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//{
+//    int n;
+//    Console.WriteLine("Eded daxil edin.");
+//    n = int.Parse(Console.ReadLine());
+
+//    if (n % 7 == 0 && n % 3 == 0)
+//    {
+
+//        Console.WriteLine("True");
+//    }
+
+//    else
+//    {
+//        Console.WriteLine("False");
+//    }
+
+
+//}
+
+//int n = 12;
+//int m = 18;
+//{
+//    if (n % 2 == 0 && m % 2 == 0)
+//    {
+//        Console.WriteLine(n + m);
+//    }
+
+
+
+//else
+//        Console.WriteLine("False");
+//}
+
+
+
+//{
+
+//    int n = 1;
+//    int m = 20;
+//    int sum = 0;
+
+//    for (int i = n + 1; i < m; i++)
+//    {
+//        if (i % 2 == 1 && n < m)
+//        {
+
+//            sum += i;
+
+//        }
+
+//    }
+//    Console.WriteLine(sum);
+
+//}
+
+
+
+
+//{
+//    int[] num = { 1, 2, 3, 4, 5, 11, 17, 23, 77, 88, 48, 58, 71 };
+//    int sum = 0;
+
+//    for (int i = 0; i < num.Length; i++)
+
+//    {
+//        if (num[i] % 2 == 1)
+//        {
+//            sum += num[i];
+
+//        }
+//    }
+//    Console.WriteLine(sum);
+//}
+
+//{
+//    int[] num = { 1, 2, 3, 4, 5, 11, 17, 23, 77, 88, 48, 58, 71 };
+//    int count = 0; 
+
+//    for (int i = 0; i < num.Length; i++)
+//    {
+//        if (num[i] % 2 == 0)
+//        {
+//            count++;
+//        }
+//    }
+
+//    Console.WriteLine(count);
+
+
+
+//}
+
+
+
